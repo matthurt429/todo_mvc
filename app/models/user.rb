963112eff2e79d4f1b548f956adcfce2443cfc5a
matthurt429 <matthurt429@arrayschool.com>
2017-12-07 16:42:06 -0700

    built backend

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable, :omniauthable
+
+has_many :lists
+end
